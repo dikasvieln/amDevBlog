f30664136516ecaa896bcdd4cd2ad752aaacf381
dikadika <dika@ruangguru.com>
2018-12-09 14:18:32 +0700

    updating config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 module.exports = {
   title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  author: 'Aulia Mahardika', // Required
+  description: 'Front End Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://amdikadika.ca',
+    github: 'https://github.com/amdika',
+    twitter: 'https://twitter.com/auliamdika',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://amdika.github.io/amDevBlog/',
 };
